6413674f67680be94170e5b67b5b139797b9d3d5
Veena Grace <veenagrace12@gmail.com>
2022-09-25 19:46:10 +0530

    Update workflow_v1.py

--- Orchestrate ML Pipeline/version_1/workflow_v1.py
+++ Orchestrate ML Pipeline/version_1/workflow_v1.py
@@ -1,5 +1,6 @@
 from typing import Any, Dict, List
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn.preprocessing import StandardScaler
@@ -16,7 +17,19 @@ def load_data(path: str, unwanted_cols: List) -> pd.DataFrame:
 def get_classes(target_data: pd.Series) -> List[str]:
     return list(target_data.unique())
 
-def get_ordinal(data: pd.DataFrame) -> Any:
+
+
+def split_data(input_: pd.DataFrame, output_: pd.Series, test_data_ratio: float) -> Dict[str, Any]:
+    X_tr, X_te, y_tr, y_te = train_test_split(input_, output_, test_size=test_data_ratio, random_state=0)
+    return {'X_TRAIN': X_tr, 'Y_TRAIN': y_tr, 'X_TEST': X_te, 'Y_TEST': y_te}
+
+#seperating categorical data
+def seperating_categorical(data:pd.DataFrame) -> pd.DataFrame:
+    sep_categorical = data.select_dtypes(include=['object'])
+    return sep_categorical
+
+
+def get_ordinal(data: pd.DataFrame) -> pd.DataFrame:
     # scaling the categorical features
     ord_enc = OrdinalEncoder()
     ord_enc.fit(data)
@@ -33,7 +46,12 @@ def rescale_catg_data(data: pd.DataFrame, ord_enc: Any) -> pd.DataFrame:
                                 index = data.index)
     return enc_data_rescaled
 
-def get_scaler(data: pd.DataFrame) -> Any:
+#seperating numerical data
+def seperating_numerical(data:pd.DataFrame) -> pd.DataFrame:
+    sep_numerical = data.select_dtypes(include=['int64', 'float64'])
+    return sep_numerical
+
+def get_scaler(data: pd.DataFrame) -> pd.DataFrame:
     # scaling the numerical features
     scaler = StandardScaler()
     scaler.fit(data)
@@ -50,15 +68,13 @@ def rescale_num_data(data: pd.DataFrame, scaler: Any) -> pd.DataFrame:
                                 index = data.index)
     return num_data_rescaled
 
+def concat_df(data:pd.DataFrame,data1:pd.DataFrame) -> pd.DataFrame:
+    concated_df= pd.concat([data,data1], axis=1)
+    return  concated_df
 
 
 
 
-
-
-def split_data(input_: pd.DataFrame, output_: pd.Series, test_data_ratio: float) -> Dict[str, Any]:
-    X_tr, X_te, y_tr, y_te = train_test_split(input_, output_, test_size=test_data_ratio, random_state=0)
-    return {'X_TRAIN': X_tr, 'Y_TRAIN': y_tr, 'X_TEST': X_te, 'Y_TEST': y_te}
 
 
 def find_best_model(X_train: pd.DataFrame, y_train: pd.Series, estimator: Any, parameters: List) -> Any:
@@ -108,30 +124,32 @@ def main(path: str):
     train_test_dict = split_data(input_=input_data, output_=target_data, test_data_ratio=TEST_DATA_RATIO)
 
    
+# Preprocessing X_train Data
+
+    # 1.Categorical Train Data
+    Categorical_train=seperating_categorical(train_test_dict['X_TRAIN'])
+    ord_enc = get_ordinal(Categorical_train)
+    catg_train = rescale_catg_data(Categorical_train, ord_enc=ord_enc)
+    # 2.Numerical Train Data
+    Numerical_train=seperating_numerical(train_test_dict['X_TRAIN'])
+    scaler = get_scaler(Numerical_train)
+    num_train = rescale_num_data(Numerical_train, scaler=scaler)
+
+    X_train_transformed=concat_df(catg_train,num_train)
 
 
-    # Rescaling Train and Test Categorical Data
-    ord_enc = get_ordinal(train_test_dict['X_TRAIN'].select_dtypes(include=['object']))
-    catg_train = rescale_catg_data(data=train_test_dict['X_TRAIN'].select_dtypes(include=['object']), ord_enc=ord_enc)
-    catg_test = rescale_catg_data(data=train_test_dict['X_TEST'].select_dtypes(include=['object']), ord_enc=ord_enc)
+ # Preprocessing X_test Data
+
+    # 1.Categorical Test Data
+    Categorical_test=seperating_categorical(train_test_dict['X_TEST'])
+    ord_enc = get_ordinal(Categorical_test)
+    catg_test = rescale_catg_data(Categorical_test, ord_enc=ord_enc)
+    # 2.Numerical Test Data
+    Numerical_test=seperating_numerical(train_test_dict['X_TEST'])
+    scaler = get_scaler(Numerical_test)
+    num_test = rescale_num_data(Numerical_test, scaler=scaler)
     
-    
-    catg_train = pd.DataFrame(catg_train)
-    catg_test = pd.DataFrame(catg_test)
-
-    # Rescaling Train and Test Numerical Data
-    scaler = get_scaler(train_test_dict['X_TRAIN'].select_dtypes(exclude=['object']))
-    num_train = rescale_num_data(data=train_test_dict['X_TRAIN'].select_dtypes(exclude=['object']), scaler=scaler)
-    num_test = rescale_num_data(data=train_test_dict['X_TEST'].select_dtypes(exclude=['object']), scaler=scaler)
-
-    
-    num_train = pd.DataFrame(num_train)
-    num_test = pd.DataFrame(num_test)
-
-    # Rescaling Train and Test Data
-
-    X_train_transformed = pd.concat([num_train,catg_train], axis=1)
-    X_test_transformed = pd.concat([num_test,catg_test], axis=1)
+    X_test_transformed=concat_df(catg_test,num_test)
 
 
 
@@ -141,7 +159,7 @@ def main(path: str):
               'min_samples_leaf': [1, 5, 10, 20, 35, 50, 100]}]
     regressor = find_best_model(X_train_transformed, train_test_dict['Y_TRAIN'], ESTIMATOR, HYPERPARAMETERS)
     print(regressor.best_params_)
-    
+    print(regressor.score(X_test_transformed,train_test_dict['Y_TEST']))
     
 # Run the main function
 main(path='./data/diamonds.csv')        
